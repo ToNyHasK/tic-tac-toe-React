2ab1c50014abf6498dcbb4d340b2d101b1e5e6c9
ToNyHasK <tomasd@wix.com>
2020-10-06 15:47:18 +0300

    exported Board, added useEffect hook to check for winner after state change

---
+++ src/components/Board.Component.ts
@@ -0,0 +1,18 @@
+const brd = () => [
+    [
+        { key: 1, id: '' },
+        { key: 2, id: '' },
+        { key: 3, id: '' },
+    ],
+    [
+        { key: 4, id: '' },
+        { key: 5, id: '' },
+        { key: 6, id: '' },
+    ],
+    [
+        { key: 7, id: '' },
+        { key: 8, id: '' },
+        { key: 9, id: '' },
+    ],
+];
+export default brd;
